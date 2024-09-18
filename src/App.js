0a678958ef034667938de157796096fa8d70fb62
Roman <45940547+zhekoldin@users.noreply.github.com>
2024-09-18 09:20:46 +0300

    clear prj
    Удалил все файлы для списка курсов

--- src/App.js
+++ src/App.js
@@ -1,62 +1,10 @@
-import { useState } from 'react';
 import './App.css';
-import CourseList from './CourseList/CourseList.tsx';
-
-const mockList = [
-  {
-    id: 1,
-    title: 'Hello',
-    description: 'World World World World World World World World World',
-    counter: 5,
-    is_active: true
-  },
-  {
-    id: 2,
-    title: 'Hello',
-    description: 'World World World World World World World World World',
-    counter: 5,
-    is_active: true
-  },
-  {
-    id: 3,
-    title: 'Hello',
-    description: 'World World World World World World World World World',
-    counter: 5,
-    is_active: true
-  },
-  {
-    id: 4,
-    title: 'Hello',
-    description: 'World World World World World World World World World',
-    counter: 5,
-    is_active: false
-  },
-  {
-    id: 5,
-    title: 'Hello',
-    description: 'World World World World World World World World World',
-    counter: 5,
-    is_active: true
-  },
-  {
-    id: 6,
-    title: 'Hello',
-    description: 'World World World World World World World World World',
-    counter: 5,
-    is_active: false
-  },
-]
 
 function App() {
 
   return (
     <div>
-      <header>
-        Список курсов
-      </header>
-      <main>
-        <CourseList courseList={mockList} />
-      </main>
+
     </div>
   );
 }
